ac1865c9f816eade7edd6eb6ba55f0bc48c5934c
Mohammad Alavi <m.alavi1986@gmail.com>
2024-02-02 01:34:37 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,17 @@ terraform {
 
 }
 
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.89.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  features {}
+  # Configuration options
 }
 
 resource "azurerm_resource_group" "example" {
